280ae139f094f10e62def9e86cea5c42428bf05b
AndreaChiappini <117998815+AndreaChiappini@users.noreply.github.com>
2022-12-03 01:57:50 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
+//
+// Created by Andrea Chiappini on 03/12/22.
+//
+
+#include "lib.h"
+
+int primo(int a, int b) {
+    bool x=true;
+    if (b!=1){
+        if ((a % b ) != 0) {
+            b--;
+            x = primo(a, b);
+        } else {
+            x=false;
+        }
+    } else {
+        x=true;
+    }
+    return x;
+}
 
